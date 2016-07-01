9daf0a4830b2a2c8d06bd07b4c3c6e1db8a0de66
Nick <nick.lyman@gmail.com>
2016-07-01 10:03:43 -0700

    Correctly returning user input on button click to print in console.

--- app/src/main/java/com/epicodus/weatherpit/MainActivity.java
+++ app/src/main/java/com/epicodus/weatherpit/MainActivity.java
@@ -3,25 +3,31 @@ package com.epicodus.weatherpit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mGetWeatherButton;
+    private EditText mUserInputEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mUserInputEditText = (EditText) findViewById(R.id.userInputEditText);
         mGetWeatherButton = (Button) findViewById(R.id.getWeatherButton);
-            mGetWeatherButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MainActivity.this, CurrentHistoricWeatherGraphs.class);
-                    startActivity(intent);
-                }
-            });
+        mGetWeatherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String cityState = mUserInputEditText.getText().toString();
+                Log.d(TAG, cityState);
+                Intent intent = new Intent(MainActivity.this, CurrentHistoricWeatherGraphs.class);
+                startActivity(intent);
+            }
+        });
     }
 }
