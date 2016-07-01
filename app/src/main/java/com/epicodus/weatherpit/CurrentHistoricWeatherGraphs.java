fb4868e5d5e4eab9149caeb514cf99b82d15d0d7
Nick <nick.lyman@gmail.com>
2016-07-01 11:40:51 -0700

    Successfully pass userInput intent.putExtra to CurrentHistoricWeatherGraphs.java and display data using getIntent().

--- app/src/main/java/com/epicodus/weatherpit/CurrentHistoricWeatherGraphs.java
+++ app/src/main/java/com/epicodus/weatherpit/CurrentHistoricWeatherGraphs.java
@@ -1,13 +1,22 @@
 package com.epicodus.weatherpit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class CurrentHistoricWeatherGraphs extends AppCompatActivity {
+    private TextView mLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_historic_weather_graphs);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        Intent intent = getIntent();
+        String cityState = intent.getStringExtra("cityState");
+        mLocationTextView.setText(cityState);
     }
 }
