1ef0734747adc544425fea92882d8a556d52b4c9
Nick <nick.lyman@gmail.com>
2016-07-01 15:29:47 -0700

    Added text for linking to API website at bottom of two pages using service, but link is not currently taking user to website. Need to fix.

--- app/src/main/java/com/epicodus/weatherpit/CurrentHistoricWeatherGraphs.java
+++ app/src/main/java/com/epicodus/weatherpit/CurrentHistoricWeatherGraphs.java
@@ -3,6 +3,7 @@ package com.epicodus.weatherpit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class CurrentHistoricWeatherGraphs extends AppCompatActivity {
+    private TextView mForecastLinkTextView;
     private TextView mLocationTextView;
     private Button mSevenDayForecastButton;
 
@@ -17,8 +19,10 @@ public class CurrentHistoricWeatherGraphs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_historic_weather_graphs);
+        mForecastLinkTextView = (TextView) findViewById(R.id.forecastLinkTextView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
         mSevenDayForecastButton = (Button) findViewById(R.id.sevenDayForecastButton);
+        mForecastLinkTextView.setText(Html.fromHtml("Powered by Forecast<a href=\"http://forecast.io/\"></a>"));
         Intent intent = getIntent();
         String cityState = intent.getStringExtra("cityState");
         mLocationTextView.setText(cityState);
--- app/src/main/java/com/epicodus/weatherpit/SevenDayForecastActivity.java
+++ app/src/main/java/com/epicodus/weatherpit/SevenDayForecastActivity.java
@@ -2,10 +2,13 @@ package com.epicodus.weatherpit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class SevenDayForecastActivity extends AppCompatActivity {
+    private TextView mForecastLinkTextView;
     private ListView mForecastListView;
     private String[] forecast = new String[] {"Monday - ICON, 56° - 78°", "Tuesday - ICON, 52° - 76°", "Wednesday - ICON, 48° - 75°", "Thursday - ICON, 58° - 82°", "Friday - ICON, 64° - 88°", "Saturday - ICON, 62° - 84°", "Sunday - ICON, 60° - 78°"};
 
@@ -13,9 +16,11 @@ public class SevenDayForecastActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seven_day_forecast);
+        mForecastLinkTextView = (TextView) findViewById(R.id.forecastLinkTextView);
         mForecastListView = (ListView) findViewById(R.id.forecastListView);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, forecast);
         mForecastListView.setAdapter(adapter);
+        mForecastLinkTextView.setText(Html.fromHtml("Powered by Forecast<a href=\"http://forecast.io/\"></a>"));
 
     }
 }
