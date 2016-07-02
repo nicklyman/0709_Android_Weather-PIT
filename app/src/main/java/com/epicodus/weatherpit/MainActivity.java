7893527a04da1375a64584c46491201f4f60bc43
Nick <nick.lyman@gmail.com>
2016-07-01 17:11:08 -0700

    Added button with link to AboutApp page from MainActivity.

--- app/src/main/java/com/epicodus/weatherpit/MainActivity.java
+++ app/src/main/java/com/epicodus/weatherpit/MainActivity.java
@@ -14,6 +14,7 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button mGetWeatherButton;
+    private Button mAboutAppButton;
     private EditText mUserInputEditText;
     private TextView mTitleTextView;
 
@@ -26,7 +27,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mTitleTextView.setTypeface(oswaldFont);
         mUserInputEditText = (EditText) findViewById(R.id.userInputEditText);
         mGetWeatherButton = (Button) findViewById(R.id.getWeatherButton);
+        mAboutAppButton = (Button) findViewById(R.id.aboutAppButton);
         mGetWeatherButton.setOnClickListener(this);
+        mAboutAppButton.setOnClickListener(this);
     }
 
     @Override
@@ -35,6 +38,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String cityState = mUserInputEditText.getText().toString();
             Intent intent = new Intent(MainActivity.this, CurrentHistoricWeatherGraphs.class);
             intent.putExtra("cityState", cityState);
+            startActivity(intent);
+        }
+        if(view == mAboutAppButton) {
+            Intent intent = new Intent(MainActivity.this, AboutAppActivity.class);
             startActivity(intent);
         }
     }
