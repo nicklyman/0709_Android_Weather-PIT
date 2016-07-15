378690e2417a3aa3cd906d5b76008bd032865ea3
Nick <nick.lyman@gmail.com>
2016-07-14 21:16:40 -0700

    Adjust styling of weather icons and text in Fragment.

--- app/src/main/java/com/epicodus/weatherpit/ui/DailyForecastDetailFragment.java
+++ app/src/main/java/com/epicodus/weatherpit/ui/DailyForecastDetailFragment.java
@@ -77,8 +77,8 @@ public class DailyForecastDetailFragment extends Fragment implements View.OnClic
         } if(mForecast.getDailyIcon().equals("")){
             Picasso.with(view.getContext()).load(R.drawable.weather_clock_icon).into(mWeatherIconPlaceholder);
         }
-        mHighTemperature.setText("Daily High Temperature: " + Double.toString(mForecast.getDailyMaxTemp()) + "° F");
-        mLowTemperature.setText("Daily Low Temperature: " + Double.toString(mForecast.getDailyMinTemp()) + "° F");
+        mHighTemperature.setText("High Temperature:  " + Double.toString(mForecast.getDailyMaxTemp()) + "° F");
+        mLowTemperature.setText("Low Temperature:  " + Double.toString(mForecast.getDailyMinTemp()) + "° F");
 
         mAPILink.setOnClickListener(this);
 
