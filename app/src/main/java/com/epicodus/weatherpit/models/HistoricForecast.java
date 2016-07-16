c61c9e14933fdf68b495ab3a14e787f63df32176
Nick <nick.lyman@gmail.com>
2016-07-16 12:00:02 -0700

    Create HistoricForecast model and method for creating random year.

---
+++ app/src/main/java/com/epicodus/weatherpit/models/HistoricForecast.java
@@ -0,0 +1,79 @@
+package com.epicodus.weatherpit.models;
+
+
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import com.epicodus.weatherpit.ui.CurrentHistoricWeatherActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+@Parcel
+public class HistoricForecast {
+    private static final String TAG = HistoricForecast.class.getSimpleName();
+    private long mHistoricTime;
+    private String mHistoricDayOfWeek;
+    private String mHistoricDailySummary;
+    //    private String mHistoricHourlySummary;
+    private double mHistoricMinTemp;
+    private double mHistoricMaxTemp;
+    private long mHistoricTimeOffset;
+
+    public HistoricForecast() {}
+
+    public HistoricForecast(long historicTime, String historicDailySummary, double minHistoricTemp, double maxHistoricTemp, long historicTimeOffset) {
+        this.mHistoricTime = historicTime;
+        this.mHistoricDailySummary = historicDailySummary;
+//    this.mHistoricHourlySummary = historicHourlySummary;
+        this.mHistoricMinTemp = minHistoricTemp;
+        this.mHistoricMaxTemp = maxHistoricTemp;
+        this.mHistoricTimeOffset = historicTimeOffset;
+    }
+
+    public long getHistoricUnixTime() {
+        return mHistoricTime;
+    }
+
+    public String getHistoricDailySummary() {
+        return mHistoricDailySummary;
+    }
+
+//    public String getHistoricHourlySummary() { return mHistoricHourlySummary; }
+
+    public double getHistoricDailyMinTemp() {
+        return mHistoricMinTemp;
+    }
+
+    public double getHistoricDailyMaxTemp() {
+        return mHistoricMaxTemp;
+    }
+
+    public long getHistoricTimeOffset() { return mHistoricTimeOffset; }
+
+
+    public long getRandomYear() {
+        long timeOffset = mHistoricTimeOffset * (-1);
+        long timeOffsetSeconds = timeOffset * 3600;
+        long unixSeconds =  (mHistoricTime + timeOffsetSeconds) * 1000L;
+        long randomYearMath = (long) ((Math.random()*59)+1) * 31536000;
+        long randomYear = unixSeconds - randomYearMath;
+        return randomYear;
+    }
+}
+
+
+
+
+
